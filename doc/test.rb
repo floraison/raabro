802df3fc81713f7b02f99dd0beaa021e4764e066
John Mettraux <jmettraux@gmail.com>
2015-12-26 11:02:50 +0900

    keep doc/test.rb around

---
+++ doc/test.rb
@@ -0,0 +1,53 @@
+
+#
+# A sample raabro parser/rewriter
+#
+# Thu Sep 24 06:18:57 JST 2015
+#
+
+require 'raabro'
+
+
+module Fun include Raabro
+
+  # parse
+  #
+  # Last function is the root, "i" stands for "input".
+
+  def pa(i); rex(nil, i, /\(\s*/); end
+  def pz(i); rex(nil, i, /\)\s*/); end
+  def com(i); rex(nil, i, /,\s*/); end
+
+  def num(i); rex(:num, i, /-?[0-9]+\s*/); end
+
+  def args(i); eseq(:args, i, :pa, :exp, :com, :pz); end
+  def funame(i); rex(:funame, i, /[a-z][a-z0-9]*/); end
+  def fun(i); seq(:fun, i, :funame, :args); end
+
+  def exp(i); alt(:exp, i, :fun, :num); end
+
+  # rewrite
+  #
+  # Names above (:num, :fun, ...) get a rewrite_xxx function.
+  # "t" stands for "tree".
+
+  def rewrite_exp(t); rewrite(t.children[0]); end
+  def rewrite_num(t); t.string.to_i; end
+
+  def rewrite_fun(t)
+    [ t.children[0].string ] +
+    t.children[1].children.inject([]) { |a, e| a << rewrite(e) if e.name; a }
+  end
+end
+
+module Fun2 extend Fun
+end
+
+
+p Fun2.parse('mul(1, 2)')
+  # => ["mul", 1, 2]
+p Fun2.parse('mul(1, add(-2, 3))')
+  # => ["mul", 1, ["add", -2, 3]]
+p Fun2.parse('mul (1, 2)')
+  # => nil (doesn't accept a space after the function name)
+
