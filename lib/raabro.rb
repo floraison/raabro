c9c90993637010954e9ab2da6c314c5c04b481ab
John Mettraux <jmettraux@gmail.com>
2025-04-04 13:25:32 +0900

    Use 0;90 for Raabro.pp dark gray

--- lib/raabro.rb
+++ lib/raabro.rb
@@ -614,15 +614,6 @@ module Raabro
 
   make_includable
 
-    # Black       0;30     Dark Gray     1;30
-    # Blue        0;34     Light Blue    1;34
-    # Green       0;32     Light Green   1;32
-    # Cyan        0;36     Light Cyan    1;36
-    # Red         0;31     Light Red     1;31
-    # Purple      0;35     Light Purple  1;35
-    # Brown       0;33     Yellow        1;33
-    # Light Gray  0;37     White         1;37
-
   def self.pp(tree, depth=0, opts={})
 
     fail ArgumentError.new(
@@ -633,7 +624,7 @@ module Raabro
 
     _rs, _dg, _gn, _yl, _bl, _lg =
       (opts[:colors] || opts[:colours] || $stdout.tty?) ?
-      [ "[0;0m", "[1;30m", "[0;32m", "[1;33m", "[0;34m", "[0;37m" ] :
+      [ "[0;0m", "[0;90m", "[0;32m", "[1;33m", "[0;34m", "[0;37m" ] :
       [ '', '', '', '', '', '' ]
 
     lc = tree.result == 1 ? _gn : _dg
